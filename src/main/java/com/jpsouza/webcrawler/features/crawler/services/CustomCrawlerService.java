4e6a105fbde688b2977a9adb6a5203581ba0c285
João Paulo de Souza <joao_paulo767@hotmail.com>
2023-06-08 12:23:04 -0300

    Update files to new approach using selenium

--- src/main/java/com/jpsouza/webcrawler/features/crawler/services/CustomCrawlerService.java
+++ src/main/java/com/jpsouza/webcrawler/features/crawler/services/CustomCrawlerService.java
@@ -1,35 +1,7 @@
 package com.jpsouza.webcrawler.features.crawler.services;
+import java.util.Set;
 
-import edu.uci.ics.crawler4j.crawler.CrawlConfig;
-import edu.uci.ics.crawler4j.crawler.CrawlController;
-import edu.uci.ics.crawler4j.crawler.WebCrawler;
-import edu.uci.ics.crawler4j.fetcher.PageFetcher;
-import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
-import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
-import org.springframework.stereotype.Service;
-
-import java.io.File;
-import java.util.ArrayList;
-
-@Service
-public class CustomCrawlerService extends WebCrawler {
-    private static final String crawlStorageFolder = "/tmp/crawler4j/";
-    private CrawlController controller;
-
-    public void startCrawler(int numberOfCrawlers, ArrayList<String> urls) throws Exception {
-        CrawlConfig config = new CrawlConfig();
-        config.setCrawlStorageFolder(CustomCrawlerService.crawlStorageFolder);
-        File folder = new File(config.getCrawlStorageFolder());
-        PageFetcher pageFetcher = new PageFetcher(config);
-        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
-        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
-        controller = new CrawlController(config, pageFetcher, robotstxtServer);
-        urls.forEach(controller::addSeed);
-        CrawlController.WebCrawlerFactory<CustomCrawlerService> factory = CustomCrawlerService::new;
-        controller.startNonBlocking(factory, numberOfCrawlers);
-    }
-
-    public void stopCrawler() {
-        controller.shutdown();
-    }
+public interface CustomCrawlerService {
+    void startCrawler(int crawlersNumber, Set<String> urls);
+    void stopCrawler();
 }
---
+++ src/main/java/com/jpsouza/webcrawler/features/crawler/services/CustomCrawlerServiceImplementation.java
@@ -0,0 +1,52 @@
+package com.jpsouza.webcrawler.features.crawler.services;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class CustomCrawlerServiceImplementation implements CustomCrawlerService {
+
+    WebDriver driver;
+
+    @Override
+    public void startCrawler(int crawlersNumber, Set<String> urls) {
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        for (String url : urls) {
+            driver.get(url);
+            List<WebElement> productContainers = driver.findElements(new By.ByXPath("//div[@class=\"slick-slide slick-active\"]"));
+            for (WebElement productContainer : productContainers) {
+                Actions action = new Actions(driver);
+                action.moveToElement(productContainer).perform();
+                WebElement linkElement = productContainer.findElement(new By.ByXPath("//a[@class=\"ui-item__link\"]"));
+                WebElement image = productContainer.findElement(new By.ByXPath("//img[@class=\"ui-item__image\"]"));
+                WebElement price = productContainer.findElement(new By.ByXPath("//div[@class=\"ui-item__price-and-discount-wrapper\"]"));
+                WebElement priceInstallments = productContainer.findElement(new By.ByXPath("//span[@class=\"ui-item__installments\"]"));
+                WebElement title = productContainer.findElement(new By.ByXPath("//p[@class=\"ui-item__title\"]"));
+                System.out.println(title.getText());
+                System.out.println(price.getText());
+                System.out.println(priceInstallments.getText());
+                System.out.println(image.getAttribute("src"));
+                System.out.println(linkElement.getAttribute("href"));
+                action.release();
+                return;
+            }
+
+        }
+        // Close the WebDriver and release resources
+        //driver.quit();
+    }
+
+    @Override
+    public void stopCrawler() {
+    }
+}
