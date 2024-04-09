b737e55d7beb69be8b589064658721a9907d2477
João Paulo de Souza <joao_paulo767@hotmail.com>
2024-04-09 11:55:36 -0300

    Atualizado o pom

--- classifier_service/src/main/java/com/jpsouza/webcrawler/ClassifierServiceApplication.java
+++ classifier_service/src/main/java/com/jpsouza/webcrawler/ClassifierServiceApplication.java
@@ -1,10 +1,5 @@
 package com.jpsouza.webcrawler;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,14 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
-import com.jpsouza.webcrawler.callables.OntologyCallable;
-import com.jpsouza.webcrawler.configuration.MongoConfig;
-import com.jpsouza.webcrawler.dtos.PossibleProductDTO;
-import com.jpsouza.webcrawler.enums.SimilarityMethod;
-import com.jpsouza.webcrawler.models.Product;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -29,18 +17,5 @@ import com.jpsouza.webcrawler.models.Product;
 public class ClassifierServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ClassifierServiceApplication.class, args);
-		MongoTemplate template = new MongoConfig().mongoTemplate();
-		PossibleProductDTO possibleProductDTO = new PossibleProductDTO();
-		possibleProductDTO.setDescription("Refrigerante Antarctica Garrafa 2 5l");
-		Callable<Product> callable = new OntologyCallable(possibleProductDTO, SimilarityMethod.COSINE, template);
-		Future<Product> future = Executors.newFixedThreadPool(1).submit(callable);
-		try {
-			Product product = future.get();
-			System.out.println(product);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} catch (ExecutionException e) {
-			e.printStackTrace();
-		}
 	}
 }
