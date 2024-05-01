8f79bb005a52e0fa62bfb0ad3a3b4bf24ce6c2a4
João Paulo de Souza <joao_paulo767@hotmail.com>
2024-05-01 09:38:39 -0300

    Adicionado o serviço do eureka para registro dos microserviços

---
+++ eureka_service/src/test/java/com/jpsouza/webcrawler/eureka_service/EurekaServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jpsouza.webcrawler.eureka_service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	/*
+	 * @Test
+	 * void contextLoads() {
+	 * }
+	 */
+
+}
