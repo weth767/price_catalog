1653d234d4f704b5311167e111d44aa5c0f67ddf
João Paulo de Souza <joao_paulo767@hotmail.com>
2024-05-01 09:39:40 -0300

    Adicionado o serviço do gateway

---
+++ gateway_service/src/test/java/com/jpsouza/webcrawler/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jpsouza.webcrawler.gateway_service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	/*
+	 * @Test
+	 * void contextLoads() {
+	 * }
+	 */
+
+}
